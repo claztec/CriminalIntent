cfb7adf42fc7e60a0429ce6fd20f32288a98b463
Jaeman Choi <claztec@gmail.com>
2016-08-07 15:26:12 +0900

    add button

--- app/src/main/java/net/claztec/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/net/claztec/android/criminalintent/CrimeFragment.java
@@ -8,7 +8,14 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
  * Created by claztec on 2016. 8. 1..
@@ -16,6 +23,8 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,6 +53,19 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, MMM, d, yyyy", Locale.US);
+        mDateButton.setText(simpleDateFormat.format(mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
 
