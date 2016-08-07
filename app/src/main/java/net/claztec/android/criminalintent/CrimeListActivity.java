435fb1182afbb7deea12062804acc332efb8501f
Jaeman Choi <claztec@gmail.com>
2016-08-07 16:30:01 +0900

    Add recyclerView

---
+++ app/src/main/java/net/claztec/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package net.claztec.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by claztec on 2016. 8. 7..
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
