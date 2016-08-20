de12690b34c74fc2d22e15512144a4f398ed117c
Jaeman Choi <claztec@gmail.com>
2016-08-20 13:33:02 +0900

    Add menu

--- app/src/main/java/net/claztec/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/net/claztec/android/criminalintent/SingleFragmentActivity.java
@@ -3,13 +3,13 @@ package net.claztec.android.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by claztec on 2016. 8. 7..
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
